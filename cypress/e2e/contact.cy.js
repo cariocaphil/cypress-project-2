f2f4e6f337c9f758184da8ce06097da81ea4aacc
cariocaphil <cariocaphil@gmail.com>
2023-04-19 22:58:40 +0200

    possibility

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -3,7 +3,7 @@ describe('contact form', () => {
       cy.visit('http://localhost:5173/about')
       cy.get('[data-cy="contact-input-message"]').type('Hello World');
       cy.get('[data-cy="contact-input-name"]').type('John Doe');
-      cy.get('[data-cy="contact-input-email"]').type('test@example.com');
+      cy.get('[data-cy="contact-input-email"]').type('test@example.com{enter}');
       cy.get('[data-cy="contact-btn-submit"]').contains('Send Message').should('not.have.attr', 'disabled');
       cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
       cy.get('@submitBtn').click();
